f337ae242f7624c38c19fe655dd9990dc1a250c3
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-03-26 13:56:40 +0000

    Create primary React app

---
+++ webpack.common.js
@@ -0,0 +1,28 @@
+const CleanWebPackPlugin = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+    entry: {
+        main: './src/index.js'
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve('./public')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: ['/node_modules'],
+                use: [{ loader: 'babel-loader' }]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'index.html'
+        }),
+        new CleanWebPackPlugin()
+    ]
+}
