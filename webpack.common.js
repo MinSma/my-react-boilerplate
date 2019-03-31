be2bf75e193d6f89b23a83dcd829e58aaf172673
Mindaugas <mindaugas.smalinskas@gmail.com>
2019-03-31 16:44:55 +0100

    Change to work with .jsx files

--- webpack.common.js
+++ webpack.common.js
@@ -4,17 +4,20 @@ const path = require('path')
 
 module.exports = {
     entry: {
-        main: './src/index.js'
+        main: './src/index.jsx'
     },
     output: {
         filename: '[name].[hash].js',
         path: path.resolve('./public')
     },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: ['/node_modules'],
+                test: /\.jsx?$/,
+                exclude: [/node_modules/],
                 use: [{ loader: 'babel-loader' }]
             }
         ]
